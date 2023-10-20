c91ef0ceba1e7ca5e364af51e41be2b52c1a82d2
termz-test <xfigures6@gmail.com>
2023-10-20 02:43:10 +0100

    Navigate next Month

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const currentDate = document.querySelector(".current-date");
 const daySelect = document.querySelector(".days");
+const iconSelect = document.querySelectorAll("icons, span");
 let today = new Date(),
 currYear = today.getFullYear();
 currMonth = today.getMonth();
@@ -9,11 +10,20 @@ const allMonths = ["January", "February", "March", "April", "May", "June", "July
 const renderCalendar = () =>{
     let liTag ="";
     let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+
     for (let i = 1; i <= lastDateofMonth; i++) {
             liTag += `<li>${i}</li>`
-        console.log(i)
-    }
+    };
+
+
     currentDate.innerHTML = `${allMonths[currMonth]} ${currYear}`;
     daySelect.innerHTML = liTag;
 }
 renderCalendar();
+
+iconSelect.forEach(icons => {
+    icons.addEventListener("click", () => {
+        currMonth = icons.id === "prev" ? currMonth - 1 : currMonth + 1;
+        renderCalendar();
+    })
+});
