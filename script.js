b5a85d70c93e3fd96cc1655922ca475079f179b9
termz-test <xfigures6@gmail.com>
2023-10-20 02:10:51 +0100

    adding dates dynamically

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const currentDate = document.querySelector(".current-date");
-
+const daySelect = document.querySelector(".days");
 let today = new Date(),
 currYear = today.getFullYear();
 currMonth = today.getMonth();
@@ -7,6 +7,13 @@ currMonth = today.getMonth();
 const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const renderCalendar = () =>{
+    let liTag ="";
+    let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+    for (let i = 1; i <= lastDateofMonth; i++) {
+            liTag += `<li>${i}</li>`
+        console.log(i)
+    }
     currentDate.innerHTML = `${allMonths[currMonth]} ${currYear}`;
+    daySelect.innerHTML = liTag;
 }
 renderCalendar();
