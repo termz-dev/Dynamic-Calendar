450aec93691df8f9e90c834170a33b6b42a9a1c8
termz-test <xfigures6@gmail.com>
2023-10-20 03:03:41 +0100

    Adding inactive days from different Months

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ const allMonths = ["January", "February", "March", "April", "May", "June", "July
 const renderCalendar = () =>{
     let liTag ="";
     let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+    firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+    console.log(lastDateofLastMonth);
+
+    for (let i = firstDayofMonth; i > 0; i--) {
+         liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`
+        
+    }
 
     for (let i = 1; i <= lastDateofMonth; i++) {
             liTag += `<li>${i}</li>`
