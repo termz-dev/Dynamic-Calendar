282c6a0e50271e503d72de51c99fab381b9b6606
termz-test <xfigures6@gmail.com>
2023-10-25 15:13:38 +0100

    final commit

--- script.js
+++ script.js
@@ -9,19 +9,28 @@ const allMonths = ["January", "February", "March", "April", "May", "June", "July
 
 const renderCalendar = () =>{
     let liTag ="";
+    let isToday ="";
     let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
     firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
     lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
-    console.log(lastDateofLastMonth);
+    firstDayofNextMonth = new Date(currYear, currMonth + 1, 1).getDay();
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay()
+ 
 
     for (let i = firstDayofMonth; i > 0; i--) {
-         liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`
+             liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
         
     }
 
     for (let i = 1; i <= lastDateofMonth; i++) {
-            liTag += `<li>${i}</li>`
+       isToday = i === new Date().getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "" ;
+            liTag += `<li class="${isToday}">${i}</li>`;
     };
+
+    for (let i = lastDayofMonth; i < 6; i++) {
+            liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+        
+    }
 
 
     currentDate.innerHTML = `${allMonths[currMonth]} ${currYear}`;
@@ -32,6 +41,15 @@ renderCalendar();
 iconSelect.forEach(icons => {
     icons.addEventListener("click", () => {
         currMonth = icons.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+        if (currMonth < 0 || currMonth > 11) {
+            today = new Date(currYear, currMonth);
+            currYear = today.getFullYear();
+            currMonth = today.getMonth();
+        } else {
+            today = new Date();
+        }
+
         renderCalendar();
     })
 });
